844c63d602a3aae576137f9492c9e2d4cccf87fa
surinder0322@gmail.com <sunny@Sunnys-Laptop.local>
2023-11-07 22:49:06 -0500

    added ifelse

---
+++ nov7/IfElseDemo.java
@@ -0,0 +1,21 @@
+package nov7;
+
+public class IfElseDemo {
+    public static void main(String[] args) {
+
+        // AWS/Devops ? to this
+        // AWS - services which we need to use
+        // EC2, ECS, s3 bucket, RDS[main], intro - SQS/SNS IAM, lambda etc
+
+
+        // Full Stack ----- Any programming[Java] C# backend/ FE
+        // deployment -- > you will learn docker, kubernetes , AWS
+
+        // BE  - Java , Spring springboot
+        // FE  -- Javascript, typescript, React or Angular
+        // Full stack -- BE
+        //
+
+
+    }
+}
---
+++ nov7/Solution.java
@@ -0,0 +1,15 @@
+package nov7;
+
+public class Solution {
+    public static void main(String[] args) {
+
+        int x = 10;
+        if(x % 2 == 0 && x > 10){
+            System.out.println(" X is even ");
+        }
+        else {
+            System.out.println("X is odd ");
+        }
+
+    }
+}
