cf408a3d40a1e37d4d9f68df715312129aaf7141
surinder0322@gmail.com <sunny@Sunnys-Laptop.local>
2023-11-23 20:29:36 -0500

    added task A

--- nov23/Test.java
+++ nov23/Test.java
@@ -4,5 +4,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Git practice");
         System.out.println("New Task B ");
+        System.out.println("Unfinished Task A "); // 90% of task is done
+        // there is a task B
+
     }
 }
