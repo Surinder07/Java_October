4f6e42f38cee98540a7b927bc998636d57372b17
surinder0322@gmail.com <sunny@Sunnys-Laptop.local>
2023-11-28 22:20:06 -0500

    String assignment

--- nov28/StringMore.java
+++ nov28/StringMore.java
@@ -4,20 +4,20 @@ public class StringMore {
     public static void main(String[] args) {
 
         String str = "   Hello World    ";
-        /*String str2 = "hello world";
+        String str2 = "hello world";
         String substring = str.substring(2,6);
         System.out.println(substring);
         System.out.println(str.equalsIgnoreCase(str2));
         String replace = str.replace('H', 'T');
         System.out.println(replace);
 
-        System.out.println(str.indexOf('l'));*/
+        System.out.println(str.indexOf('l'));
         System.out.println(str.trim());
         System.out.println(str);
 
         System.out.println(str.isEmpty());
 
-       /* System.out.println(str.toUpperCase());
+        System.out.println(str.toUpperCase());
 
         char c = str.charAt(6);
         System.out.println(c);
@@ -29,15 +29,9 @@ public class StringMore {
         for (char e: chars) {
             System.out.println(e);
         }
-
-*/
-
-
-
         // mutable
-       /* StringBuilder stringBuilder = new StringBuilder("test");
+        StringBuilder stringBuilder = new StringBuilder("test");
         System.out.println(stringBuilder.reverse());
-*/
         // can you reverse this String = "Pragra" ?
     }
 }
