6d5a75958b56c5939f531a6e2f1886a4dfac1e3e
surinder0322@gmail.com <sunny@Sunnys-Laptop.local>
2023-12-05 20:42:09 -0500

    multi level inheritance

---
+++ dec5/Test.java
@@ -0,0 +1,20 @@
+package dec5;
+
+public class Test {
+    public static void main(String[] args) {
+
+
+
+    }
+}
+/*
+recap
+1. inheritance
+2. pillars of object oriented programming
+---- encapsulatin, polymorhism and abstraction
+3.  StringBuffer StringBuilder String
+4. super keyword , this keyword
+5. access modifiers
+ // super
+        // variable , method , constructor
+ */
---
+++ dec5/Vehicle.java
@@ -0,0 +1,89 @@
+package dec5;
+
+public class Vehicle {
+    String color;
+    String model;
+
+    public Vehicle(String color, String model) {
+        this.color = color;
+        this.model = model;
+    }
+
+    public void printDetails(){
+        System.out.println("Color : "+color);
+        System.out.println("model : "+model);
+    }
+}
+
+class Car extends Vehicle{
+
+    String bodyType;
+
+    public Car(String color, String model, String bodyType) {
+        super(color, model);
+        this.bodyType = bodyType;
+    }
+
+    public void displayCar(){
+        System.out.println(bodyType);
+    }
+}
+
+class  Hyundai extends Car{
+
+    String engineType;
+
+    public Hyundai(String color, String model, String bodyType, String engineType) {
+        super(color, model, bodyType);
+        this.engineType = engineType;
+    }
+
+    public void displayHyundai(){
+        System.out.println(color);
+        System.out.println(model);
+        System.out.println(engineType);
+    }
+}
+
+class VehicleDemo {
+    public static void main(String[] args) {
+
+       /* Car car = new Car("Red", "Honda","Sedan");
+        car.printDetails();
+        Vehicle vehicle = new Vehicle("Blue","Tesla");
+        vehicle.printDetails();*/
+        Hyundai hyundai = new Hyundai("Green","Elantra","Sedan","Petrol");
+        hyundai.displayHyundai();
+        hyundai.printDetails();
+        hyundai.displayCar();
+
+    }
+}
+
+/*
+multiv level
+A
+|
+B
+|
+C*
+
+          Heirarchical                   multiple [not in java class]
+           A                            A show(String a)              B show(String a)
+         |  |                                                |
+        B   C                                                C
+                                        C c = new C();
+                                         c.show();  // Diamond problem
+
+
+              Hierachical :
+              BankAccount : String accountHolder, double  balance
+              method : displayInfo
+
+              Savings                        Checking Account
+              double interest rate           double overdraft limit
+
+                      Savings        currentAccount
+
+/
+ */
